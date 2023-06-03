d4f041fdcac057f187b764640d9da7441287175b
schmiseb <z3bbie@gmail.com>
2023-06-03 16:06:55 +0200

    Added opening enrichment of geojson

--- geocoding/enrich.py
+++ geocoding/enrich.py
@@ -55,17 +55,13 @@ class EnrichGeoJSON:
 
     def add_e4_d4_split(self):
         '''
-        Get the output DataFrame from _calculate_e4d4_split and add it to the countries.geojson
+        Add the average share of E4 and D4 openings played per country of origin. Also add a position indicating if
+        E4 or D4 dominates in comparison with the global averages.
         '''
         opening_df = self._calculate_e4d4_split()
 
-        self.meta_json['d4_positions'] = opening_df.loc[opening_df['norm_e4_share'] <= 0]['position'].max() - 1
+        self.meta_json['d4_positions'] = opening_df.loc[opening_df['e4_share_standardized'] <= 0]['position'].max() - 1
         self.meta_json['e4_positions'] = opening_df['position'].max() - self.meta_json['d4_positions']
-
-        print(self.meta_json['d4_positions'])
-        print(opening_df.loc[opening_df['norm_e4_share'] <= 0])
-        print(self.meta_json['e4_positions'])
-        print(opening_df.loc[opening_df['norm_e4_share'] > 0])
 
         opening_dict = {
             country: (position, e4, d4) for country, position, e4, d4
@@ -88,11 +84,17 @@ class EnrichGeoJSON:
 
     def _calculate_e4d4_split(self) -> pd.DataFrame:
         '''
-        Assigns each country a percentage for e4 and d4 openings.
+        Function to create a DataFrame for the E4/D4 split on country level.
+        - Calculate the average share of E4 and D4 openings played per country of origin
+        - Calculate the relative E4 share (only considering E4 and D4 share)
+        - Standardize the E4 share to have negative values for countries with D4 and positive values for countries
+          with E4 preference
+        - Assign positions to each country that will be used in color coding
+
         Currently averages on player level, meaning that the number of games per player is not regarded
         (TODO: Potentially update this)
         Dummy function; Will be used to add actual E4/D4 information per country to the countries.geojson
-        :return:    A DataFrame with three columns: norm_e4_share, country_e4, country_d4
+        :return:    A DataFrame with four columns: e4_share_standardized, position, country_e4, country_d4
         '''
         # TODO: Remove in favor of correct approach
         import random
@@ -108,20 +110,89 @@ class EnrichGeoJSON:
         country_d4 = self.player_df.groupby('ISO_A3')['d4'].mean()
 
         # TODO: The following code can probably stay the same (Confirm)
-        # Bring values on one scale and normalize using mean and std:
+        # Bring values on one scale and standardize using mean and std:
         # - Positive values: e4 is more popular -> Number stored in num_e4
         # - Negative values: d4 is more popular -> Number stored in num_d4
         e4_share = country_e4 / (country_e4 + country_d4)
-        norm_e4_share = ((e4_share - e4_share.mean()) / e4_share.std()).sort_values()
+
+        # TODO: Calculate mean and standard deviation over all games instead of aggregated by player and by country
+        e4_share_standardized = ((e4_share - e4_share.mean()) / e4_share.std()).sort_values()
 
         # Get the position corresponding to a normalized share
-        positions = self._assign_pos_to_share(share_series=norm_e4_share)
+        positions = self._assign_pos_to_share(share_series=e4_share_standardized)
 
         # Combine all information into a single df
-        opening_df = pd.concat([norm_e4_share, positions, country_e4, country_d4], axis=1)
-        opening_df.rename(columns={0: 'norm_e4_share'}, inplace=True)
+        opening_df = pd.concat([e4_share_standardized, positions, country_e4, country_d4], axis=1)
+        opening_df.rename(columns={0: 'e4_share_standardized'}, inplace=True)
         opening_df.rename(columns={1: 'position'}, inplace=True)
 
+        return opening_df
+
+
+    def add_opening(self, name: str ='SICILIAN_DEFENSE'):
+        '''
+        Add the average probability for an opening played per country of origin. Also add a position indicating if
+        the probability is higher or lower than the global average.
+        '''
+        opening_df = self._calculate_opening(name)
+
+        self.meta_json['openings'] = self.meta_json.get('openings', []) + [name]
+        self.meta_json[f'{name}_neg_positions'] = opening_df.loc[opening_df['standardized_prob'] <= 0]['position'].max() - 1
+        self.meta_json[f'{name}_pos_positions'] = opening_df['position'].max() - self.meta_json[f'{name}_neg_positions']
+
+        opening_dict = {
+            country: (position, prob) for country, position, prob
+            in zip(opening_df.index,
+                   opening_df.position,
+                   opening_df[name])
+                        }
+
+        # Add the opening information to each country
+        for country_feature in tqdm.tqdm(self.country_json['features'], desc=f'Adding {name} probabilities to countries'):
+            properties = country_feature['properties']
+
+            # If no information is available for a country, return 0, 0
+            country_tuple = opening_dict.get(properties['ISO_A3'], (0, 0))
+            properties[f'{name}_POS'] = country_tuple[0]
+            properties[name] = country_tuple[1]
+
+
+    def _calculate_opening(self, name: str ='SICILIAN_DEFENSE') -> pd.DataFrame:
+        '''
+        Function to create a DataFrame for the probability of a specific opening on country level.
+        - Calculate the probability for that opening being played per country of origin
+        - Standardize the probability to have negative values for countries with lower probability than global average
+          and positive values for countries with a higher probability
+        - Assign positions to each country that will be used in color coding
+
+        Currently averages on player level, meaning that the number of games per player is not regarded
+        (TODO: Potentially update this)
+        Dummy function; Will be used to add actual opening information per country to the countries.geojson
+        :return:    A DataFrame with three columns: standardized_prob, position, [name]
+        '''
+        # TODO: Remove in favor of correct approach
+        import random
+        def random_prob(row):
+            row[name] = max(0.05, min(0.20, random.gauss(0.125, 0.05)))
+            return row
+
+        self.player_df = self.player_df.apply(random_prob, axis=1)
+        prob = self.player_df.groupby('ISO_A3')[name].mean()
+
+        # TODO: The following code can probably stay the same (Confirm)
+        # Bring values on one scale and standardize using mean and std:
+        # - Positive values: Opening is more popular than global average
+        # - Negative values: Opening is less popular than global average
+
+        # TODO: Calculate mean and standard deviation over all games instead of aggregated by player and by country
+        standardized_prob = ((prob - prob.mean()) / prob.std()).sort_values()
+
+        # Get the position corresponding to a normalized share
+        positions = self._assign_pos_to_share(share_series=standardized_prob)
+
+        # Combine all information into a single df
+        opening_df = pd.concat([standardized_prob, positions, prob], axis=1)
+        opening_df.columns = ['standardized_prob', 'position', name]
         return opening_df
 
     @staticmethod
@@ -165,4 +236,9 @@ if __name__ == '__main__':
 
     enricher.add_player_count()
     enricher.add_e4_d4_split()
+
+    openings = ['SICILIAN_DEFENSE', 'LATVIAN_GAMBIT', 'PIZZA_MAFIA_MANDOLINO']
+    for opening in openings:
+        enricher.add_opening(opening)
+
     enricher.save_geojson()
